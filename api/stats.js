841e6d89492d9da954f8a5e6994de830e1cab111
Zenko27 <161458848+Zenko27@users.noreply.github.com>
2025-05-17 22:26:32 +0200

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   try {
     const apiUrl = `https://api.brawlstars.com/v1/players/%23${encodeURIComponent(tag)}`;
     const response = await fetch(apiUrl, {
-      headers: { Authorization: 'Bearer YOUR_API_TOKEN' }
+      headers: { Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNlYjI2Nzk1LTkxZWEtNDEyMy1iMzYyLWJmMzAyYmI5ZjQ5MSIsImlhdCI6MTc0NzM5MDQzNCwic3ViIjoiZGV2ZWxvcGVyL2RlZmZmY2VkLTdjYjYtZjM3NC1mMmE1LWNhOWE0ZjFiYjJiOSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTg4LjE2NS4zMy4xMzMiXSwidHlwZSI6ImNsaWVudCJ9XX0.um6ftlTZ7KJkadj0FcBskL75bAUJeRQQr1EHNCrtYVp3ISvcqRvhCpQ4ugPLIUvvBe2lJnRX7YjI9UKnBdMusg' }
     });
 
     if (!response.ok) {
